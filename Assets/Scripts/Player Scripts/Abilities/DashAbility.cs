17923dbcef85253403408246013830c69bfbc567
SamuelSchnee <99436589+SamuelSchnee@users.noreply.github.com>
2022-05-11 11:52:25 -0500

    tried to get working dash (didn't work)

--- Assets/Scripts/Player Scripts/Abilities/DashAbility.cs
+++ Assets/Scripts/Player Scripts/Abilities/DashAbility.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class DashAbility : Ability
 {
     public Rigidbody2D playerRb;
+    public bool dashing;
+    Vector2 dashDistance;
+    public PlayerController playerCnt;
+
+    public Transform dashDestination;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerCnt = GetComponent<PlayerController>();
+        playerRb = GetComponent<Rigidbody2D>();
         cooldownLength = 20.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(cooldownLength <= 0)
+        {
+            readyToUse = true;
+        }
+
         if(Input.GetKeyDown(KeyCode.Tab) && readyToUse == true)
         {
-            playerRb.velocity = Vector2.zero;
+            dashing = true;
+        }
+
+        if(dashing == true)
+        {
+            playerCnt.canMove = false;
+            Invoke("Dash", 2);
         }
     }
 
     private void Dash()
     {
-        
+        Vector2 destination = dashDestination.transform.position;
+        transform.position = destination;
+        dashing = false;
+        playerCnt.canMove = true;
     }
 }
